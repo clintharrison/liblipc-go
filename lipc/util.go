0e72a51e70087ebff6807116f92d10232b895dab
Sighery <11218602+Sighery@users.noreply.github.com>
2025-12-08 00:36:20 +0100

    Sync with latest bueno changes (#1)
    Co-authored-by: Clint Harrison <243356+clintharrison@users.noreply.github.com>

--- lipc/util.go
+++ lipc/util.go
@@ -2,8 +2,7 @@ package lipc
 
 import "github.com/godbus/dbus/v5"
 
-// this is useful for debugging...
-func nameForHeaderField(t dbus.HeaderField) string {
+func NameForHeaderField(t dbus.HeaderField) string {
 	switch t {
 	case dbus.FieldPath:
 		return "Path"
@@ -27,4 +26,53 @@ func nameForHeaderField(t dbus.HeaderField) string {
 	return "Unknown"
 }
 
-// TODO: LIPC status to error string (LipcGetErrorString in liblipc)
+func NameForLipcError(errnum uint32) string {
+	switch errnum {
+	case 0:
+		return "lipcErrNone"
+	case 1:
+		return "lipcErrUnknown"
+	case 2:
+		return "lipcErrInternal"
+	case 3:
+		return "lipcErrNoSuchSource"
+	case 4:
+		return "lipcErrOperationNotSupported"
+	case 5:
+		return "lipcErrOutOfMemory"
+	case 6:
+		return "lipcErrSubscriptionFailed"
+	case 7:
+		return "lipcErrNoSuchParam"
+	case 8:
+		return "lipcErrNoSuchProperty"
+	case 9:
+		return "lipcErrAccessNotAllowed"
+	case 0xa:
+		return "lipcErrBufferTooSmall"
+	case 0xb:
+		return "lipcErrInvalidHandle"
+	case 0xc:
+		return "lipcErrInvalidArg"
+	case 0xd:
+		return "lipcErrOperationNotAllowed"
+	case 0xe:
+		return "lipcErrParamsSizeExceeded"
+	case 0xf:
+		return "lipcErrTimedOut"
+	case 0x10:
+		return "lipcErrServiceNameTooLong"
+	case 0x11:
+		return "lipcErrDuplicateServiceName"
+	case 0x12:
+		return "lipcErrInitDBus"
+	case 0x100:
+		return "lipcPropErrInvalidState"
+	case 0x101:
+		return "lipcPropErrNotInitialized"
+	case 0x102:
+		return "lipcPropErrInternal"
+	default:
+		return "Unknown"
+	}
+}
